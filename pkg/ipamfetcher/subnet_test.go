ce67b9ff8ae6f71e2a6ed27cf5d1b85142b2ee32
Zachary Lozano <zlozano@atlassian.com>
2019-07-29 10:41:12 -0500

    account for missing customer id in subnet response

--- pkg/ipamfetcher/subnet_test.go
+++ pkg/ipamfetcher/subnet_test.go
@@ -48,6 +48,28 @@ func TestFetchSubnetsMultiple(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+func TestFetchSubnetsMissingCustomerID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockPageFetcher := NewMockPageFetcher(ctrl)
+	mockPageFetcher.EXPECT().FetchPage(gomock.Any(), 0, 1).Return(PagedResponse{TotalCount: 3, Offset: 0, Body: []byte(`{"offset": 0, "limit": 1, "total_count": 3, "subnets": [{"subnet_id": 1, "network": "192.168.1.1", "mask_bits": 32, "custom_fields": [{"key": "Location", "value": "AUS"}], "customer_id": 1}]}`)}, nil)
+	mockPageFetcher.EXPECT().FetchPage(gomock.Any(), 1, 1).Return(PagedResponse{TotalCount: 3, Offset: 1, Body: []byte(`{"offset": 1, "limit": 1, "total_count": 3, "subnets": [{"subnet_id": 2, "network": "192.168.1.0", "mask_bits": 28, "custom_fields": [{"key": "Location", "value": "SYD"}], "customer_id": 2}]}`)}, nil)
+	mockPageFetcher.EXPECT().FetchPage(gomock.Any(), 2, 1).Return(PagedResponse{TotalCount: 3, Offset: 2, Body: []byte(`{"offset": 2, "limit": 1, "total_count": 3, "subnets": [{"subnet_id": 3, "network": "192.168.1.3", "mask_bits": 32, "custom_fields": [{"key": "Location", "value": "LON"}], "customer_id": 0}]}`)}, nil)
+
+	d := &Device42SubnetFetcher{
+		Limit:       1,
+		PageFetcher: mockPageFetcher,
+		LogFn:       testLogFn,
+	}
+
+	subnets, err := d.FetchSubnets(context.Background())
+	assert.ElementsMatch(t, []domain.Subnet{
+		domain.Subnet{ID: "1", Network: "192.168.1.1", MaskBits: int8(32), Location: "AUS", CustomerID: "1"},
+		domain.Subnet{ID: "2", Network: "192.168.1.0", MaskBits: int8(28), Location: "SYD", CustomerID: "2"},
+	}, subnets)
+	assert.Nil(t, err)
+}
+
 func TestFetchSubnetsUnmarshalError(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
