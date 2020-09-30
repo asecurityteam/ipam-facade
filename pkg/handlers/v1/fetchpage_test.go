7ad44c2914fcbf5dc7a76535a72e559330cccb68
Mike Rheinheimer <mrheinheimer@atlassian.com>
2020-09-30 10:54:33 -0500

    fix pagination to indicate we've reached the last page so callers know

--- pkg/handlers/v1/fetchpage_test.go
+++ pkg/handlers/v1/fetchpage_test.go
@@ -41,8 +41,11 @@ func TestFetchSubnets(t *testing.T) {
 	subnets := result.Result.([]Subnet)
 	require.Equal(t, len(expectedSubnets), len(subnets))
 
+	// fewer subnets than the limit were returned; there are no more pages
+	require.Equal(t, "", result.NextPageToken)
+	// pageFromToken func should still be safe to call, so we check that it is
 	pr, _ := pageFromToken(result.NextPageToken)
-	require.Equal(t, limit+offset, pr.Offset)
+	require.Equal(t, 0, pr.Offset)
 }
 
 func TestFetchSubnetsDefaultLimit(t *testing.T) {
@@ -73,6 +76,46 @@ func TestFetchSubnetsDefaultLimit(t *testing.T) {
 	subnets := result.Result.([]Subnet)
 	require.Equal(t, len(expectedSubnets), len(subnets))
 
+	// fewer subnets than the limit were returned; there are no more pages
+	require.Equal(t, "", result.NextPageToken)
+	// pageFromToken func should still be safe to call, so we check that it is
+	pr, _ := pageFromToken(result.NextPageToken)
+	require.Equal(t, 0, pr.Offset)
+}
+
+func TestFetchSubnetsDefaultLimitMorePages(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	limit := 2
+	offset := 0
+
+	expectedSubnets := []domain.AssetSubnet{
+		{
+			Network: "0.0.0.0/32",
+		},
+		{
+			Network: "0.0.0.0/32",
+		},
+	}
+
+	mockFetcher := NewMockFetcher(ctrl)
+	mockFetcher.EXPECT().FetchSubnets(gomock.Any(), limit, offset).Return(expectedSubnets, nil)
+
+	h := &FetchPageHandler{
+		Fetcher:         mockFetcher,
+		LogFn:           testLogFn,
+		DefaultPageSize: limit,
+	}
+	result, err := h.FetchSubnets(context.Background(), PaginationRequest{})
+
+	require.NoError(t, err)
+
+	subnets := result.Result.([]Subnet)
+	require.Equal(t, len(expectedSubnets), len(subnets))
+
+	// an equal number of subnets to the limit were returned; there may be more pages
+	require.Equal(t, "PMRGY2LNNF2CEORSFQRG6ZTGONSXIIR2GJ6Q", result.NextPageToken)
 	pr, _ := pageFromToken(result.NextPageToken)
 	require.Equal(t, limit+offset, pr.Offset)
 }
@@ -127,8 +170,11 @@ func TestFetchIPs(t *testing.T) {
 	ips := result.Result.([]IP)
 	require.Equal(t, len(expectedIPs), len(ips))
 
+	// fewer subnets than the limit were returned; there are no more pages
+	require.Equal(t, "", result.NextPageToken)
+	// pageFromToken func should still be safe to call, so we check that it is
 	pr, _ := pageFromToken(result.NextPageToken)
-	require.Equal(t, limit+offset, pr.Offset)
+	require.Equal(t, 0, pr.Offset)
 }
 
 func TestFetchIPsDefaultLimit(t *testing.T) {
@@ -161,8 +207,50 @@ func TestFetchIPsDefaultLimit(t *testing.T) {
 
 	fmt.Println(result.NextPageToken)
 
+	// fewer subnets than the limit were returned; there are no more pages
+	require.Equal(t, "", result.NextPageToken)
+	// pageFromToken func should still be safe to call, so we check that it is
 	pr, _ := pageFromToken(result.NextPageToken)
-	require.Equal(t, limit+offset, pr.Offset)
+	require.Equal(t, 0, pr.Offset)
+}
+
+func TestFetchIPsDefaultLimitMorePages(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	limit := 2
+	offset := 0
+
+	expectedIPs := []domain.AssetIP{
+		{
+			IP: "0.0.0.0/32",
+		},
+		{
+			IP: "0.0.0.0/32",
+		},
+	}
+
+	mockFetcher := NewMockFetcher(ctrl)
+	mockFetcher.EXPECT().FetchIPs(gomock.Any(), limit, offset).Return(expectedIPs, nil)
+
+	h := &FetchPageHandler{
+		Fetcher:         mockFetcher,
+		LogFn:           testLogFn,
+		DefaultPageSize: limit,
+	}
+	result, err := h.FetchIPs(context.Background(), PaginationRequest{})
+
+	require.NoError(t, err)
+
+	ips := result.Result.([]IP)
+	require.Equal(t, len(expectedIPs), len(ips))
+
+	fmt.Println(result.NextPageToken)
+
+	// an equal number of subnets to the limit were returned; there may be more pages
+	require.Equal(t, "PMRGY2LNNF2CEORSFQRG6ZTGONSXIIR2GJ6Q", result.NextPageToken)
+	pr, _ := pageFromToken(result.NextPageToken)
+	require.Equal(t, 2, pr.Offset)
 }
 
 func TestFetchIPsError(t *testing.T) {
